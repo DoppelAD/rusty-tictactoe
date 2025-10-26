576470e1dd8672a06a19ccc719b5a81c28c116ca
Alper <aad@doppelad.de>
2025-10-26 19:02:44 +0100

    feat(board): implement core game mechanics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+mod colors;
+mod board;
+mod player;
+
+use board::Board;
+use player::Player;
+
+use crate::colors::{colored_text, GREEN, PURPLE};
+
 fn main() {
-    println!("Hello, world!");
+    let mut board = Board::new(3);
+    println!("{}\n RuStY TiCTaCToE! \n{}\n{}", colored_text("==================", PURPLE), colored_text("==================", PURPLE), board);
+
+    for column in 0..3 {
+        match board.make_move(0, column, Player::X) {
+            Ok(_) => println!("\nRound {} \n{}", column + 1,  board),
+            Err(e) => println!("Error at row {}: {:?}", column, e),
+        }
+
+        match board.check_winner() {
+            Some(player) => println!("{}\n Winner: {}\n{}", colored_text("===========", GREEN), player, colored_text("===========", GREEN)),
+            None => {},
+        }
+    }
 }
+

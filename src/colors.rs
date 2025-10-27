ced43c5f7930c29476ac6b19c96ee41ae80124c6
Alper <aad@doppelad.de>
2025-10-27 21:02:53 +0100

    feat: clear terminal each round

--- src/colors.rs
+++ src/colors.rs
@@ -4,6 +4,13 @@ pub const GREEN: &str = "\x1b[32m";
 pub const BLUE: &str = "\x1b[34m";
 pub const PURPLE: &str = "\x1b[35m";
 
+use std::io::{self, Write};
+
 pub fn colored_text(text: &str, color: &str) -> String {
     format!("{}{}{}", color, text, RESET)
+}
+
+pub fn clear_console() {
+    print!("\x1B[2J\x1B[1;1H");
+    io::stdout().flush().unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,35 @@ mod board;
 mod player;
 mod position;
 
+use std::io;
+
 use game::Game;
+use colors::clear_console;
 
 fn main() {
-    let mut game = Game::new();
-    game.start_game();
+    loop {
+        let mut game = Game::new();
+        
+        clear_console();
+        game.start_game();
+
+        if !ask_for_rematch() {
+            break
+        }
+    }
 }
 
+fn ask_for_rematch() -> bool {
+        let mut player_input = String::new();
+
+        println!("Do you want to play another round? (y/n):");
+        
+        io::stdin()
+            .read_line(&mut player_input)
+            .expect("Failed to read line!");
+
+        match player_input.trim().to_lowercase().as_str() {
+            "y" => return true,
+            _ => return false,
+        }
+    }
